ca9fb209919ae5ac7eb0efbeac37c951ca5cf332
Solomon Ashagre <solomonashagre1398@gmail.com>
2026-09-06 15:51:18 +0300

    Update env vars

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer');
 
+const port = Number(process.env.EMAIL_PORT) || 587;
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 587,
-  secure: false, // true for port 465, false for 587 (STARTTLS)
+  port,
+  secure: port === 465, // true for 465 (implicit SSL), false for 587 (STARTTLS)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
